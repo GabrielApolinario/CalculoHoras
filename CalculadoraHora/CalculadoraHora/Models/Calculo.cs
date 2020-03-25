d2cce17008fd422ecf31db2ff848148555eefeec
Gabriel <gab.apolinario@hotmail.com>
2020-03-25 11:10:17 -0300

    First Commit

---
+++ CalculadoraHora/CalculadoraHora/Models/Calculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraHora.Models
+{
+    public class Calculo
+    {
+
+        public int idTransaction { get; set; }
+
+        public double result { get; set; }
+
+        public DateTime executionHour { get; set; }
+
+    }
+}
